fb05f6681203cbf54b663db107aed49de5426a9c
syidev99 <syidev99@gmail.com>
2022-10-08 21:48:01 +0900

    REDUX-PARCEL

---
+++ vanilla-redux/index.js
@@ -0,0 +1,12 @@
+const divToggle = document.querySelector(".toggle");
+const counter = document.querySelector("h1");
+const btnIncrease = document.querySelector("#increase");
+const btnDecrease = document.querySelector(".decrease");
+
+const TOGGLE_SWITCH = "TOGGLE_SWITCH";
+const INCREASE = "INCREASE";
+const DECREASE = "DECREASE";
+
+const toggleSwitch = () => ({ type: TOGGLE_SWITCH });
+
+console.log("hello parcel");
